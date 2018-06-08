9ba8e3b4bed4ef3789808297f64cc4f47cb80884
Michael Boniface <mjb@it-innovation.soton.ac.uk>
2018-06-08 00:12:27 +0000

    readded docker

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -22,6 +22,7 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/dmcache"
 	_ "github.com/influxdata/telegraf/plugins/inputs/dns_query"
 	_ "github.com/influxdata/telegraf/plugins/inputs/dovecot"
+        _ "github.com/influxdata/telegraf/plugins/inputs/docker"
 	_ "github.com/influxdata/telegraf/plugins/inputs/elasticsearch"
 	_ "github.com/influxdata/telegraf/plugins/inputs/exec"
 	_ "github.com/influxdata/telegraf/plugins/inputs/fail2ban"
